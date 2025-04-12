bb21391a534755ac2d2c4d0fc5a9ec77f320e8b2
Luis Apaza <luis.apaza24@gmail.com>
2025-04-11 20:43:24 -0500

    add

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 import os
 
@@ -12,13 +14,27 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = 'LqxYrxsqmVFeQIOKjESbMNGUM2CXXeqC'
 
 
-# Inicializamos la base de datos
+# Inicializamos las dependencias
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 
 # Modelos
+class Usuario(db.Model):
+    __tablename__ = 'Usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    
+    def check_password(self, password):
+        bcrypt = Bcrypt(app)
+        return bcrypt.check_password_hash(self.password, password)
+
+
 class Categoria(db.Model):
     __tablename__ = 'Categoria'
     id = db.Column(db.Integer, primary_key=True)
@@ -41,6 +57,24 @@ def index():
     return "Bienvenido"
 
 
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    usuario = Usuario.query.filter_by(email=username).first()
+    # Validamos si existe el usuario o la contraseña es incorrecta
+    if not usuario or not usuario.check_password(password):
+        return({
+            'message': 'Credenciales incorrectas'
+        })
+    # Generamos el token de acceso
+    token = create_access_token(identity=str(usuario.email))
+    return({
+            'token': token
+        })
+
+
 @app.route('/api/categorias', methods=['GET'])
 def categorias_listar():
     categorias = Categoria.query.all()
@@ -49,9 +83,10 @@ def categorias_listar():
 
 
 @app.route('/api/productos', methods=['GET'])
+@jwt_required()
 def productos_listar():
     productos = Producto.query.all()
-    resultado = [{'id':p.id, 'nombre':p.nombre, 'categoria':p.categoria.nombre} for p in productos]
+    resultado = [{'id':p.id, 'nombre':p.nombre} for p in productos]
     return jsonify(resultado)
 
 
